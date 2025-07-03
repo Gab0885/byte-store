96fc722463011d90f06a2c85bf3e8b75cb00c782
Gabriel Dias <gabrieldg0885@gmail.com>
2025-07-03 18:36:11 -0300

    feat: rota de procurar produto por nome adicionada

--- server/src/routes/product.router.ts
+++ server/src/routes/product.router.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { findAllProducts, findProductByID} from "../controllers/ProdutcController";
+import { findAllProducts, findProductByID, findProductByName} from "../controllers/ProdutcController";
 
 const router = Router();
 
 router.get("/products", findAllProducts);
 
+router.get("/products/search/:name", findProductByName)
+
 router.get("/products/:id", findProductByID);
+
 
 export { router };

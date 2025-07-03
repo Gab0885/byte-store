f3bebfaa84ec5e53f04b5fc2169f667858ca36fc
Gabriel Dias <gabrieldg0885@gmail.com>
2025-07-03 17:39:21 -0300

    fix: backend agora usa o middleware de erro

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,16 @@
-import express, { type Request, type Response } from "express";
+import express from "express";
 import { router as healthcheck } from "./routes/test";
 import { router as productRouter} from "./routes/product.router"
+import { errorHandlerMiddleware as errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/healthcheck", healthcheck);
-app.use("/products", productRouter)
+app.use("/api", productRouter)
+
+app.use(errorHandler)
 
 export default app;

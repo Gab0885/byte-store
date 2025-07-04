b34dfa97ddd6091fb06c6c9b112a1d73f3afa33d
Gabriel Dias <gabrieldg0885@gmail.com>
2025-07-04 18:31:58 -0300

    feat: adicionado schema para atualização de produtos

--- server/src/schemas/product.ts
+++ server/src/schemas/product.ts
@@ -9,4 +9,15 @@ export const createProductSchema = z.object({
     quantity: z.number().nonnegative("A quantidade não pode ser negativa.")
 })
 
+export const updateProductSchema = z.object({
+    category: z.string().min(1, "Ter ao menos uma categoria é obrigatório.").optional(), 
+    description: z.string().min(1, "O produto precisa de uma descrição.").optional(), 
+    image: z.string().url("A imagem deve ser uma URL válida.").optional(), 
+    name: z.string().min(1, "O produto precisa de um nome").optional(), 
+    price: z.number().positive("O preço precisa ser maior que zero").optional(), 
+    quantity: z.number().nonnegative("A quantidade não pode ser negativa.").optional()
+})
+
 export type createProductInput = z.infer<typeof createProductSchema>
+
+export type updateProductInput = z.infer<typeof updateProductSchema>

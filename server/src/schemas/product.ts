177b2905b622c23850c9aedfe28f645c83c766f2
Gabriel Dias <gabrieldg0885@gmail.com>
2025-07-07 16:10:05 -0300

    refactor: melhorada a estruturação e logica do productSchema

--- server/src/schemas/product.ts
+++ server/src/schemas/product.ts
@@ -1,23 +1,72 @@
 import z from "zod";
 
-export const createProductSchema = z.object({
-    category: z.string().min(1, "Ter ao menos uma categoria é obrigatório."), 
-    description: z.string().min(1, "O produto precisa de uma descrição."), 
-    image: z.string().url("A imagem deve ser uma URL válida."), 
-    name: z.string().min(1, "O produto precisa de um nome"), 
-    price: z.number().positive("O preço precisa ser maior que zero"), 
-    quantity: z.number().nonnegative("A quantidade não pode ser negativa.")
-})
+export const createProductSchema = z
+  .object({
+    name: z
+      .string()
+      .nonempty("O nome do produto é obrigatório")
+      .min(2, "O nome deve ter ao menos 2 caracteres"),
+
+    category: z
+      .string()
+      .trim()
+      .nonempty("A categoria é obrigatória")
+      .min(2, "A categoria deve ter ao menos 2 caracteres"),
+
+    description: z
+      .string()
+      .trim()
+      .nonempty("A descrição é obrigatória")
+      .min(10, "A descrição deve ter ao menos 10 caracteres"),
+
+    image: z
+      .string()
+      .trim()
+      .nonempty("A URL da imagem é obrigatória")
+      .url("Formato de URL inválido"),
+
+    price: z.number().positive("O preço deve ser maior que zero"),
+
+    quantity: z.number().nonnegative("A quantidade não pode ser negativa"),
+  })
+  .strict();
 
 export const updateProductSchema = z.object({
-    category: z.string().min(1, "Ter ao menos uma categoria é obrigatório.").optional(), 
-    description: z.string().min(1, "O produto precisa de uma descrição.").optional(), 
-    image: z.string().url("A imagem deve ser uma URL válida.").optional(), 
-    name: z.string().min(1, "O produto precisa de um nome").optional(), 
-    price: z.number().positive("O preço precisa ser maior que zero").optional(), 
-    quantity: z.number().nonnegative("A quantidade não pode ser negativa.").optional()
-})
+  name: z
+    .string()
+    .nonempty("O nome do produto é obrigatório")
+    .min(2, "O nome deve ter ao menos 2 caracteres")
+    .optional(),
 
-export type createProductInput = z.infer<typeof createProductSchema>
+  category: z
+    .string()
+    .trim()
+    .nonempty("A categoria é obrigatória")
+    .min(2, "A categoria deve ter ao menos 2 caracteres")
+    .optional(),
 
-export type updateProductInput = z.infer<typeof updateProductSchema>
+  description: z
+    .string()
+    .trim()
+    .nonempty("A descrição é obrigatória")
+    .min(10, "A descrição deve ter ao menos 10 caracteres")
+    .optional(),
+
+  image: z
+    .string()
+    .trim()
+    .nonempty("A URL da imagem é obrigatória")
+    .url("Formato de URL inválido")
+    .optional(),
+
+  price: z.number().positive("O preço deve ser maior que zero").optional(),
+
+  quantity: z
+    .number()
+    .nonnegative("A quantidade não pode ser negativa")
+    .optional(),
+});
+
+export type createProductInput = z.infer<typeof createProductSchema>;
+
+export type updateProductInput = z.infer<typeof updateProductSchema>;
--- server/src/schemas/user.ts
+++ server/src/schemas/user.ts
@@ -4,6 +4,7 @@ export const createUserSchema = z
   .object({
     name: z
       .string()
+      .trim()
       .nonempty("O nome é obrigatório")
       .min(2, "O nome deve ter ao menos 2 caracteres"),
 
@@ -13,30 +14,26 @@ export const createUserSchema = z
       .nonempty("O email é obrigatório")
       .email("Formato de email inválido"),
 
-    passwordHash: z
-    .string()
-    .min(5, "A senha deve ter ao menos 5 caracteres"),
+    passwordHash: z.string().min(5, "A senha deve ter ao menos 5 caracteres"),
   })
   .strict();
 
-export const updateUserSchema = z.object({
-  name: z
-    .string()
-    .trim()
-    .min(2, "O nome deve ter ao menos 2 caracteres")
-    .optional(),
+export const updateUserSchema = z
+  .object({
+    name: z
+      .string()
+      .trim()
+      .min(2, "O nome deve ter ao menos 2 caracteres")
+      .optional(),
 
-  email: z
-  .string()
-  .trim()
-  .email("Formato de email inválido")
-  .optional(),
+    email: z.string().trim().email("Formato de email inválido").optional(),
 
-  passwordHash: z
-    .string()
-    .min(6, "A senha deve ter ao menos 6 caracteres")
-    .optional(),
-}).strict();
+    passwordHash: z
+      .string()
+      .min(6, "A senha deve ter ao menos 6 caracteres")
+      .optional(),
+  })
+  .strict();
 
 export type createUserInput = z.infer<typeof createUserSchema>;
 

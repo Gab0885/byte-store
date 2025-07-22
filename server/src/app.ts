7ba4695697280cd213c21b6ce55a40ce94814e59
Gabriel Dias <gabrieldg0885@gmail.com>
2025-07-22 19:16:23 -0300

    feat: implementado router de autenticação no sistema

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import { router as healthcheck } from "./routes/test";
 import productRouter from "./routes/product.router"
 import userRouter from "./routes/user.router"
-import  errorHandler from "./middlewares/errorHandler";
+import authRouter from "./routes/auth.router"
+import errorHandler from "./middlewares/errorHandler";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/healthcheck", healthcheck);
 app.use("/api/products", productRouter)
 app.use("/api/users", userRouter)
+app.use("/api/auth", authRouter)
 
 app.use(errorHandler)
 
